09cd153303bdeab80a0c67be2e96b6ea7e9ffc5d
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2023-04-03 18:53:20 +0300

    add EventRestController change pom.xml

--- src/main/java/com/DmitryElkin_Servlets_REST_API/controller/EventRestControllerV1.java
+++ src/main/java/com/DmitryElkin_Servlets_REST_API/controller/EventRestControllerV1.java
@@ -1,13 +1,21 @@
 package com.DmitryElkin_Servlets_REST_API.controller;
 
+import com.DmitryElkin_Servlets_REST_API.repository.utils.PrepareDB;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "EventRestControllerV1", value = "/EventRestControllerV1")
+@WebServlet(name = "EventRestControllerV1", value = "/api/v1/events/*")
 public class EventRestControllerV1 extends HttpServlet {
+    
+
+    @Override
+    public void init() {
+        PrepareDB.doPrepare();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
